99a58b83657c79ef19b23bef7faf0126cba50ae7
ShanKonduru <ShanKonduru@gmail.com>
2023-09-27 21:26:27 +0530

    reading it from file rather than hardcoding

--- preprocess.py
+++ preprocess.py
@@ -1,17 +1,25 @@
+# preprocess.py
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from gensim import corpora
 import gensim
+import pandas as pd
 
-# Sample documents (replace with your data)
-documents = [
-    "Topic modeling is a powerful technique for analyzing text data.",
-    "Natural language processing is an exciting field in AI.",
-    "Gensim is a Python library for topic modeling.",
-    "Latent Dirichlet Allocation is a popular topic modeling algorithm.",
-    "NLP helps computers understand and generate human language.",
-]
+# # Sample documents (replace with your data)
+# documents = [
+#     "Topic modeling is a powerful technique for analyzing text data.",
+#     "Natural language processing is an exciting field in AI.",
+#     "Gensim is a Python library for topic modeling.",
+#     "Latent Dirichlet Allocation is a popular topic modeling algorithm.",
+#     "NLP helps computers understand and generate human language.",
+# ]
+
+# Read data from the CSV file
+data = pd.read_csv("reviews_data.csv")
+
+# Extract the "Review" column as your documents
+documents = data["Review"].tolist()
 
 # Preprocess the documents
 nltk.download('stopwords')
